8a95a8a8961eb76b27f31b4eb436bac9e2913c84
Charindu Thisara <imcthisara@gmail.com>
2023-12-26 12:45:37 +0530

    Fix import case in App.jsx

--- onos/src/App.jsx
+++ onos/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
-import ShowTable from './pages/ShowTable'
+import ShowTable from './pages/showTable'
 import DeleteFlow from './pages/DeleteFlow'
 import CreateFlow from './pages/CreateFlow'
 
